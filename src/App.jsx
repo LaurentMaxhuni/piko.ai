bea0093495aea677cbb59405f0a18c5e02f64c3b
LaurentMaxhuni <152623037+LaurentMaxhuni@users.noreply.github.com>
2025-11-23 22:54:54 +0100

    Started

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import {Navbar} from "./components"
+
+const App = () => {
+    return (
+        <Navbar />
+    )
+}
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { css } from "../../styled-system/css";
+import { circle, stack } from "../../styled-system/patterns";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="flex items-center content-center gap-5">
+                <img />
+                <h1 className="">Piko.ai</h1>
+                <ul className="flex gap-2 items-center">
+                    <li>
+                        <a href="#home">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li>
+                        <img />
+                    </li>
+                    <li>
+                        <img />
+                    </li>
+                    <li>
+                        <img />
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
